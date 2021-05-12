689ac756be9547f4d887538cd757d700451bc465
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-05-12 10:47:33 +0100

    Create Events model and foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_103049) do
+ActiveRecord::Schema.define(version: 2021_05_12_094425) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "title"
+    t.string "Location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

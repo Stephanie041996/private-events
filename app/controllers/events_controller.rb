950b40793fde8a01f7dc97bd17c3461d05b1a4aa
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-05-14 15:38:04 +0200

    Add past and future events on index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,8 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     @events = Event.all
+    @future_events = Event.future
+    @past_events = Event.past
   end
 
   # GET /events/1 or /events/1.json
